b9b5be30da75b61b6e1fe01ceb62c57b45f1227d
Paul Gruenbacher <pgruenbacher@totalsim.us>
2016-09-10 14:45:16 -0400

    s

--- stl/base.py
+++ stl/base.py
@@ -141,10 +141,7 @@ class BaseMesh(logger.Logged, collections.Mapping):
         self.data = data
 
         points = self.points = data['vectors']
-        self.points.shape = data.size, 9
-        self.x = points[:, Dimension.X::3]
-        self.y = points[:, Dimension.Y::3]
-        self.z = points[:, Dimension.Z::3]
+        self.points = points.reshape(-1, points.shape[-1])
         self.v0 = data['vectors'][:, 0]
         self.v1 = data['vectors'][:, 1]
         self.v2 = data['vectors'][:, 2]
@@ -154,6 +151,18 @@ class BaseMesh(logger.Logged, collections.Mapping):
 
         if calculate_normals:
             self.update_normals()
+
+    @property
+    def x(self):
+        return self.points[:, 0]
+
+    @property
+    def y(self):
+        return self.points[:, 1]
+
+    @property
+    def z(self):
+        return self.points[:, 2]
 
     @classmethod
     def remove_duplicate_polygons(cls, data, value=RemoveDuplicates.SINGLE):
